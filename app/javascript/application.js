614ff542c8f5f51aa364966ea7b09dab7b6286d5
Patrick Stark <contact@patrick-stark.com>
2022-11-08 10:27:19 +0900

    added on click events

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,23 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+// DOMs
+const letterBoxes = document.querySelectorAll('.letterbox');
+const inputField = document.getElementById('form-input');
+
+
+letterBoxes.forEach((letterBox) => {
+  const letter = letterBox.innerText;
+
+
+  letterBox.addEventListener('click', (event) => {
+    // Toggle Active class on button press
+    letterBox.classList.toggle('active');
+
+    // Put button content into input field
+    if (letterBox.classList.contains('active')) {
+      inputField.value += letter;
+    }
+  });
+});
